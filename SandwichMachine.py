3adca35d4eaacafb316e8358829e13c64b561354
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-01-25 14:54:15 -0500

    feat: added ability to check reasources

--- SandwichMachine.py
+++ SandwichMachine.py
@@ -35,28 +35,3 @@ class SandwichMachine:
             print("Sorry there is not enough ham.")
             return False
         return True
-
-    @staticmethod
-    def display_resources():
-        resources = SandwichDataModel.resources
-        print(f'You have:')
-        print(f'{resources["bread"]} slices of bread.')
-        print(f'{resources["ham"]} slices of ham.')
-        print(f'{resources["cheese"]} ounces of cheese.')
-
-    def make_sandwich(self, sandwich_size):
-        size_resources = SandwichDataModel.recipes[sandwich_size]["ingredients"]
-        self.machine_resources["bread"] -= size_resources["bread"]
-        self.machine_resources["ham"] -= size_resources["ham"]
-        self.machine_resources["cheese"] -= size_resources["cheese"]
-
-    def check_all(self, sandwich_size):
-        if self.check_resources(sandwich_size):
-            sandwich_cost = SandwichDataModel.recipes[sandwich_size]["cost"]
-            if transaction_result(process_coins(), sandwich_cost):
-                self.make_sandwich(sandwich_size)
-                print(f"A {sandwich_size} sandwich was made!")
-                return
-            print("Not enough money!")
-            return
-        print("Not enough resources!")
