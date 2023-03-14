bbd646b39c05a51006d71d82d68cd350e317827d
MrCatters <73203569+MrCatters@users.noreply.github.com>
2023-03-14 17:49:38 -0400

    added assignment 5 template

--- Assignment 5 - Sandwich Maker Flask Web Application - Part1/sql_scripts/sandwich_maker_db.sql
+++ Assignment 5 - Sandwich Maker Flask Web Application - Part1/sql_scripts/sandwich_maker_db.sql
@@ -1,57 +1,48 @@
-DROP SCHEMA IF EXISTS sandwich;
-CREATE SCHEMA sandwich;
-USE sandwich;
+-- Development code:
+DROP SCHEMA IF EXISTS `sandwich_maker`;
+--
+CREATE SCHEMA IF NOT EXISTS `sandwich_maker` ;
+USE `sandwich_maker`;
 
-
-
+-- Development code:
 DROP TABLE IF EXISTS resources;
+DROP TABLE IF EXISTS sandwiches;
+DROP TABLE IF EXISTS recipes;
+--
+
 CREATE TABLE resources (
-	id smallint UNIQUE NOT NULL AUTO_INCREMENT,
+	id int UNIQUE NOT NULL AUTO_INCREMENT,
 	item varchar(50) NOT NULL,
     amount int NOT NULL,
     PRIMARY KEY (id)
 );
 
-
-
-DROP TABLE IF EXISTS sandwiches;
 CREATE TABLE sandwiches (
-	id smallint UNIQUE NOT NULL AUTO_INCREMENT,
+	id int UNIQUE NOT NULL AUTO_INCREMENT,
 	sandwich_size varchar(50) NOT NULL,
-    price decimal(5,2) NOT NULL,
+    price decimal(8,2) NOT NULL,
     PRIMARY KEY (id)
 );
 
-
-
-DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes (
-	sandwich_size smallint NOT NULL,
-    item smallint NOT NULL,
-    amount smallint NOT NULL,
+	sandwich_size int NOT NULL,
+    item int NOT NULL,
+    amount int NOT NULL,
     FOREIGN KEY (sandwich_size) REFERENCES sandwiches(id),
     FOREIGN KEY (item) REFERENCES resources(id)
 );
-
-
-
-
-
 
 INSERT INTO resources (item, amount) VALUES
 ('bread', 12),
 ('ham', 18),
 ('cheese', 24);
 
-
-
 INSERT INTO sandwiches (sandwich_size, price) VALUES
 ('small', 1.75),
 ('medium', 3.25),
 ('large', 5.5);
 
-
-
+/*
 INSERT INTO recipes VALUES
 (1, 1, 2),
 (1, 2, 4),
@@ -64,14 +55,4 @@ INSERT INTO recipes VALUES
 (3, 1, 6),
 (3, 2, 8),
 (3, 3, 12);
-
-
-
--- SELECT * FROM sandwiches;
-
--- SELECT * FROM resources;
-
-SELECT sandwiches.sandwich_size, resources.item, recipes.amount
-FROM recipes
-INNER JOIN sandwiches ON recipes.sandwich_size = sandwiches.id
-INNER JOIN resources ON recipes.item = resources.id
+*/
